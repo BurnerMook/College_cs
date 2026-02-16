4ab552f5b730d0eba871028f95411b65fe38edf0
Will <142919773+BurnerMook@users.noreply.github.com>
2026-02-16 14:02:17 -0600

    Update stuff.java

--- stuff.java
+++ stuff.java
@@ -1,1 +1,48 @@
-public 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Stuff{
+  private int[] arr;
+  private int size;
+
+  public Stuff (String fileName){
+    loadFromFile(fileName);
+  }
+      private void loadFromFile(String fileName) {
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+
+            // First pass: count numbers
+            int count = 0;
+            while (scanner.hasNextInt()) {
+                count++;
+                scanner.nextInt();
+            }
+            scanner.close();
+
+            array = new int[count];
+            size = count;
+
+            // Second pass: store numbers
+            scanner = new Scanner(new File(fileName));
+            int index = 0;
+            while (scanner.hasNextInt()) {
+                array[index++] = scanner.nextInt();
+            }
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            array = new int[0];
+            size = 0;
+        }
+    }
+  public int find(int value){
+    for(int i=0; i < size; i++){
+      if(arr[i] == value){
+        return i;
+      }
+    }
+  }
+}
+
